8edc780da04d005c26d22676e70d15b6933c9345
justclaner <justclaner@gmail.com>
2025-09-13 14:48:56 -0400

    Added timer to the Deep Dark

--- Assets/Scripts/DeepInTheDark/DeepInTheDark.cs
+++ Assets/Scripts/DeepInTheDark/DeepInTheDark.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
+using TMPro;
 
 public class DeepInTheDark : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject Player;
+
+    private float sensitivity;
+
+    private bool gameActive;
+
+    public float timer;
+
+    [SerializeField] private TMP_Text timerText;
+
+
+    [SerializeField] private GameObject startCanvas;
+    [SerializeField] private GameObject UICanvas;
+    [SerializeField] private GameObject EndScreenCanvas;
+    [SerializeField] private GameObject winText;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         // Hide the hardware cursor
         Cursor.visible = false;
+
+        sensitivity = 1f;
+        gameActive = true;
     }
 
     // Update is called once per frame
@@ -16,11 +35,25 @@ public class DeepInTheDark : MonoBehaviour
     {
         updatePlayerPosition();
         updatePlayerRotation();
+        updateTimer();
+    }
+
+    private void updateTimer()
+    {
+        if (gameActive)
+        {
+            timerText.text = $"Time Remaining: {120 - Mathf.Floor(timer)}";
+            timer += Time.deltaTime;
+            if (timer > 120f)
+            {
+                //StartCoroutine(EndGame());
+            }
+        }
     }
 
     private void updatePlayerRotation()
     {
-        Player.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+        Player.transform.Rotate(new Vector3(0, 2.5f * Input.GetAxis("Mouse X") * sensitivity, 0));
     }
 
 
@@ -51,6 +84,12 @@ public class DeepInTheDark : MonoBehaviour
         }
 
         Vector3.Normalize(velocity);
+
+        //if the player is running
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            velocity *= 2f;
+        }
 
         Player.transform.position += velocity * Time.deltaTime;
     }
