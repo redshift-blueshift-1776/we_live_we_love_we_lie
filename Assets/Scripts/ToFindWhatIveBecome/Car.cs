ffec3ddfacb41d1cbbd13fdf60ad6b2882d19fd5
Forest Ho-Chen <foresth168@gmail.com>
2025-09-27 14:38:06 -0400

    Cars

---
+++ Assets/Scripts/ToFindWhatIveBecome/Car.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    [SerializeField] public Vector3 startPosition;
+    [SerializeField] public Vector3 targetPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = startPosition;
+        transform.LookAt(targetPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/ToFindWhatIveBecome/Car_Spawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Car_Spawner : MonoBehaviour
+{
+    [SerializeField] public GameObject gameManager;
+    public ToFindWhatIveBecome gm;
+
+    [SerializeField] public GameObject car;
+
+    [SerializeField] public Vector3 startPosition;
+    [SerializeField] public Vector3 targetPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gm = gameManager.GetComponent<ToFindWhatIveBecome>();
+        StartCoroutine(spawnCars());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator spawnCars() {
+        while (true) {
+            if (gm.gameActive) {
+                Debug.Log("Spawning Car");
+                var newCar = Instantiate(car, transform);
+                newCar.SetActive(true);
+                var newCarScript = newCar.GetComponent<Car>();
+                newCarScript.targetPosition = targetPosition;
+                newCarScript.startPosition = startPosition;  
+            }
+            float waitTime = Random.Range(2f, 5f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
