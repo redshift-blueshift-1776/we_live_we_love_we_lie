d60967b8d9dbd7e13d6f1a15086c2bc1411daecd
justclaner <justclaner@gmail.com>
2025-10-12 01:48:21 -0400

    added character controller

---
+++ Assets/Scripts/ItAllBelongsToTheOtherSide/PlayerMovement7.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerMovement7 : MonoBehaviour
+{
+    public GameObject camera;
+    
+    private CharacterController characterController;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        characterController.Move(getInputDirectionVector() * Time.deltaTime);
+    }
+
+    private Vector3 getInputDirectionVector()
+    {
+        Vector3 direction = Vector3.zero;
+        Vector3 forward = new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z);
+        Vector3 right = new Vector3(camera.transform.right.x, 0, camera.transform.right.y);
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction -= right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction -= forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += right;
+        }
+        return direction.normalized;
+    }
+}
