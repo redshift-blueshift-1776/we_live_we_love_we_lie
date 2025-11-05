4fd6f8c779cf131ce2f57340189b1bf98420fb97
justclaner <justclaner@gmail.com>
2025-11-05 00:36:46 -0500

    added finite state machine template to enemy

--- Assets/Scripts/ItAllBelongsToTheOtherSide/Enemy.cs
+++ Assets/Scripts/ItAllBelongsToTheOtherSide/Enemy.cs
@@ -5,8 +5,18 @@ public class Enemy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private NavMeshAgent enemy;
     [SerializeField] private GameObject player;
+    public enum EnemyState
+    {
+        Idle,
+        Patrol,
+        Chase,
+        Attack,
+        Dead
+    }
 
     float health = 100;
+    private EnemyState currentState;
+    private float stateTimer = 0f;
     void Start()
     {
         enemy.isStopped = true;
@@ -25,5 +35,34 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void TransitionToState(EnemyState newState)
+    {
+        // Exit current state
+        OnStateExit(currentState);
+
+        // Enter new state
+        currentState = newState;
+        stateTimer = 0f;
+        OnStateEnter(newState);
+    }
+
+    private void OnStateEnter(EnemyState state)
+    {
+        switch (state)
+        {
+            //case EnemyState.Patrol:
+            //    SetNewPatrolPoint();
+            //    break;
+            //case EnemyState.Attack:
+            //    Debug.Log("Entering attack state");
+            //    break;
+        }
+    }
+
+    private void OnStateExit(EnemyState state)
+    {
+        // Cleanup when leaving a state
     }
 }
